d09aa7245673dc128e119f8e8976e9df8d50e0bd
jpwhalley <justin.whalley@gmail.com>
2023-08-01 14:47:47 -0500

    Clean up for teaaching
    Do not need the ipynb files. Corrected description in py files. Tidied new line formatting.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 2023_INSPIRE-Data_Collection_in_Python
-## Python notebooks for the 2023 INSPIRE featured presentation on data collection in python.
+## Python code for the 2023 INSPIRE featured presentation on data collection in python.
 
 
 Ideally the data we wish to work on can be downloaded in an easy to use format. Otherwise when we want only a small subset of a very big dataset, or the data is being constantly updated, hopefully the owner will provide an application programming interface (API) to automate the collection of the relevant data. However quite often the data cannot be downloaded and there is no API, but the data is publicly available, just dispersed across a website. When it would be too tedious and time consuming to navigate page by page to collect the data manually; we can use Selenium Webdriver and Beautiful Soup to automate navigating across the website and collecting of the relevant data. In this code clinic, I will go through the best practices (and what not to do!) when web scraping; using Selenium Webdriver to navigate around a website and then using Beautiful Soup to extract the data from the HTML.
@@ -8,39 +8,19 @@ Prerequisites
 -------------
 ### Please install the following suggested software:
 
-1. [Anaconda3 (for python3 notebooks)](https://www.anaconda.com/download/) or (Python3)[https://www.python.org/downloads/].  
+1. (Python3)[https://www.python.org/downloads/].  
   
-  
-2. Selenium: [https://selenium-python.readthedocs.io/](https://selenium-python.readthedocs.io/)I usually use pip or conda to install packages: 
-    
-`pip install selenium`
-  
-or
-  
-`conda install -c anaconda selenium`
-  
-  
-3. Beautiful Soup: [https://www.crummy.com/software/BeautifulSoup/bs4/doc/](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
-
-`pip install beautifulsoup4`
-
-or
-
-`conda install -c anaconda beautifulsoup4`
-  
-
-2-3. Alternatively you can install 2-3 from the requirements.txt (especially if you are working with plain python, as this will also install pandas and matplotlib for you)
+2. All the needed packages are in requirements.txt, you can install individually or:
 
 `pip install -r requirements.txt`
 
-4. Gecko drivers for my web browser [https://selenium-python.readthedocs.io/installation.html#drivers](https://selenium-python.readthedocs.io/installation.html)
-    *   You have to put the geckodriverbinary in a path Python can see, if my case I have it in ~/anaconda3/bin/ . If this does not work, I have found that putting it in your working directory (the same as the python notebook), should work.  
-  
+3. Gecko drivers for my web browser [https://selenium-python.readthedocs.io/installation.html#drivers](https://selenium-python.readthedocs.io/installation.html)
+    *   You have to put the geckodriverbinary in a path Python can see. If this does not work, I have found that putting it in your working directory (the same as the python notebook), should work.  
   
 *   Once you have done that, hopefully running the following code snippet:
 ```
 from selenium import webdriver  
-browser = webdriver.Firefox() # Or which ever web browser you are using  
+browser = webdriver.Chrome() # Or which ever web browser you are using  
 browser.get("[https://www.literaryclock.com](https://www.literaryclock.com/)/“)
 ```
 you should get a pop-up window of the [Literary Clock website](http://www.literaryclock.com/).
--- inspire_web_scraping_literaryclock.py
+++ inspire_web_scraping_literaryclock.py
@@ -1,30 +1,21 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Modelling for Global Health - Data science in Python
-# ## Day 1: Collecting disparate data online by web scraping using Selenium and BeautifulSoup
+# # INSPIRE featured presentation
+# # Collecting disparate data online by web scraping using Selenium and BeautifulSoup
 # 
 # Ideally the data we wish to work on can be downloaded in an easy to use format. Otherwise when we want only a small subset of a very big dataset, or the data is being constantly updated, hopefully the owner will provide an application programming interface (API) to automate the collection of the relevant data. However quite often the data cannot be downloaded and there is no API, but the data is publicly available, just dispersed across a website. When it would be too tedious and time consuming to navigate page by page to collect the data manually; we can use Selenium Webdriver and Beautiful Soup to automate navigating across the website and collecting of the relevant data. In this code clinic, I will go through the best practices (and what not to do!) when web scraping; using Selenium Webdriver to navigate around a website and then using Beautiful Soup to extract the data from the HTML.
 # 
 
-
 from os import path
 
-
 # ### Introduction: Why automate? 
-
 
 # Example 1 - get KIR haplotypes
 def get_hap():
     """Python file to get the KIR haplotypes:
         KIR_barcode2haplotype.py
-
         Python reference file for barcode to haplotype:
         results/allelefreqdotnet_barcode2haplotype.p
-
         Excel reference file for barcode to haplotype:
         KIR_af_haplotypes_reference.csv
-
         Results of the police men from Guinea Bissau:
         db_police_final.csv
     """
@@ -40,7 +31,6 @@ def get_hap():
     to_redo = ['1111011111111111', '1111110110111111', '1111110011011111', '1111000000011111', '1111110010111111']
     
     browser = webdriver.Firefox()
-
     # Set constants
     base_url = "http://allelefrequencies.net/kir6001a.asp?kgen_pop_selection=&kgen_group=&kgen_id=&kgen_country=&kgen_pops_pattern=equal&kgen_pops=&kgen_dataset=&kgen_region=&kgen_ethnic=&kgen_study=&kgen_order=order_1&"
     
@@ -54,7 +44,6 @@ def get_hap():
         
         browser.get(url)
         html_source = browser.page_source
-
         soup = BeautifulSoup(html_source, 'html.parser')
     
         for node in soup.findAll(attrs={'class': re.compile(r"table01")}):
@@ -74,12 +63,8 @@ def get_hap():
         # barcode2haplotype[barcode] = af_hap
         sleep(5 + (randint(1,50) / 25))
 
-
-
 # Example collecting KIR haplotypes from allelefrequencies.net
 get_hap()
-
-
 
 # Example 2 - Get Library books
 from bs4 import BeautifulSoup
@@ -88,9 +73,7 @@ from selenium import webdriver
 from time import sleep
 from random import randint
 import pandas as pd
-
 url = "https://libcat.oxfordshire.gov.uk/web/arena/results?p_p_id=crDetailWicket_WAR_arenaportlet&p_p_lifecycle=1&p_p_state=normal&p_r_p_arena_urn%3Aarena_search_item_id="
-
 # Name /Author :[arena_search_item_id]
 books = {
 "The kiwi's egg : Charles Darwin and natural selection / David Quammen" :['0297845691&p'],
@@ -98,7 +81,6 @@ books = {
 'Human compatible : artificial intelligence and the problem of control / Stuart Russell' :['0241335205&p'],
 'Fake law : the truth about justice in an age of lies / The Secret Barrister' :['1529009944&p'],
 'Somersault / Kenzaburo Oe' :['1843540800&p']}
-
 
 browser = webdriver.Firefox()
 results = {}
@@ -108,7 +90,6 @@ for book in books:
         browser.get(url + version)
         sleep(6.25 + (randint(1,50) / 25))
         html_source = browser.page_source
-
         soup = BeautifulSoup(html_source)
         
         for foo in soup.find_all('div', attrs={'class': 'arena-holding-container clearfix'}):
@@ -124,107 +105,72 @@ for book in books:
                     results[count]['Classification'] = 'Not known'
                 count += 1
         
-
 df = pd.DataFrame(results).T
-
 print(df)
 
-
 # ### Section 0: Load the packages and open a remote controlled browser
-
 
 # Import the necessary packages
 from selenium import webdriver
 import requests
 
-
-
 # Fire up Selenium webdriver with the website we want to scrape
 browser = webdriver.Firefox() # If you use a different browser, replace Firefox with this
 base_url = "https://www.literaryclock.com/"
 browser.get(base_url)
-
 # # Alternatively, use requests
 # requests.get(base_url, timeout=1) # Hopefully get Response [200], not [404]
 
-
 # ### Section 1: Navigating around using selenium
-
 # #### 1.1: Using link text
-
 
 # Start navigating around
 element = browser.find_element_by_link_text("Posts")
 print(element)
 
-
-
 element.click()
 
-
 # ### Section 2: Webscraping time
-
 # #### 2.1 Can we get a the literary works, their authors and their date
-
 
 # Navigate to Posts
 browser.find_element_by_link_text("Posts").click()
 
-
-
 # And now The Literary Clock Library
 browser.find_element_by_link_text("The Literary Clock Library").click()
-
-
 
 # Use javascript to click on the element instead
 element = browser.find_element_by_link_text("The Literary Clock Library")
 print(element)
 browser.execute_script("arguments[0].click();", element)
 
-
-
 # And now to the 1500s
 browser.find_element_by_link_text("1500s").click()
-
-
 
 # Can we grab the plays first performed in the 1500s
 from bs4 import BeautifulSoup
 from time import sleep
-
 html_source = browser.page_source
 soup = BeautifulSoup(html_source, 'html.parser')
-
 # # Alternative with requests
 # html = requests.get(base_url+'library/1500', timeout=1)
 # soup = BeautifulSoup(html.text, 'html.parser')
 
-
-
 # Looking at the 'li' tag
 print(soup.li)
 
-
-
 # OK, get all the 'li' tags
 print(soup.findAll('li'))
-
-
 
 # Look to see if it is a link or not
 for i in soup.findAll('li'):
     print(i.a)
     
 
-
-
 # So we can filter out by link (and we can also look at the link)
 for i in soup.findAll('li'):
     if i.a != None:
         print(i.a['href'])
-
-
 
 books = {}
 counter = 0
@@ -243,26 +189,19 @@ for i in soup.findAll('li'):
         
         counter +=1
 
-
-
 # Can now look at this in a dataframe
 import pandas as pd
 df = pd.DataFrame(books).T
 print(df)
 
-
-
 # OK, lets add the 1600s
 browser.find_element_by_partial_link_text("Next").click() # Nice alternative to find_element_by_link_text
-
 # Now put all of the books from this page into the "books" dictionary
 html_source = browser.page_source
 soup = BeautifulSoup(html_source, 'html.parser')
-
 # # Alternative with requests
 # html = requests.get(base_url+'library/1600', timeout=1)
 # soup = BeautifulSoup(html.text, 'html.parser')
-
 for i in soup.findAll('li'):
     if i.a == None:
         books[counter] = {}
@@ -281,14 +220,11 @@ for i in soup.findAll('li'):
 df = pd.DataFrame(books).T
 print(df)
 
-
 # #### 2.2 Automatically get all the literary works, their authors and their date
-
 
 # OK let's go back to the 1500s and try get them all
 from time import sleep
 browser.find_element_by_partial_link_text("Prev").click()
-
 carry_on = True
 books = {}
 counter = 0
@@ -318,16 +254,11 @@ while carry_on:
     except:
         carry_on=False
 
-
-
 df = pd.DataFrame(books).T
 print(df)
 
-
-
 # OK, lets check how best to collect the data
 # Can do it in two parts - first up 1500s, 1600s and 1700s
-
 books = {}
 counter = 0
 for century in ['/1500', '/1600', '/1700']:
@@ -342,25 +273,17 @@ for century in ['/1500', '/1600', '/1700']:
     for i in soup.findAll('li'):
         if i.a == None:
             books[counter] = {}
-
             book = i.string
             # print(book)
-
             books[counter]['Year'] = int(book[:4])
-
             temp = book[6:].split(' by ')
-
             books[counter]['Title'] = temp[0]
             books[counter]['Author'] = temp[1]
-
             counter +=1
     
     sleep(2)
-
 df = pd.DataFrame(books).T
 print(df)
-
-
 
 # Now for the 1800s, 1900s and 2000s
 for century in ['/1800', '/1900', '/2000']:
@@ -389,38 +312,23 @@ for century in ['/1800', '/1900', '/2000']:
         for i in soup.findAll('li'):
             if i.a == None:
                 books[counter] = {}
-
                 book = i.string
                 # print(book)
-
                 books[counter]['Year'] = int(book[:4])
-
                 temp = book[6:].split(' by ')
-
                 books[counter]['Title'] = temp[0]
                 books[counter]['Author'] = temp[1]
-
                 counter +=1
         
         sleep(2)
-
 df = pd.DataFrame(books).T
-
-
 
 # Look to quickly plot the distribution of years
 df['Year'] = df['Year'].astype('int')
 df.hist(column='Year', bins=80)
 
-
-
 df
 
-
-
 to_do
-
-
-
 
 
