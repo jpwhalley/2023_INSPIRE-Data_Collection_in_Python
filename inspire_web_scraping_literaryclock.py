1aeab2c4101eb67c65c11bd478c9d595432b8bd2
jpwhalley <justin.whalley@gmail.com>
2023-08-03 14:13:33 -0500

    Added in complete answers and fixed student code.
    Student code Firefox -> Chrome

--- inspire_web_scraping_literaryclock.py
+++ inspire_web_scraping_literaryclock.py
@@ -3,7 +3,52 @@
 # 
 # Ideally the data we wish to work on can be downloaded in an easy to use format. Otherwise when we want only a small subset of a very big dataset, or the data is being constantly updated, hopefully the owner will provide an application programming interface (API) to automate the collection of the relevant data. However quite often the data cannot be downloaded and there is no API, but the data is publicly available, just dispersed across a website. When it would be too tedious and time consuming to navigate page by page to collect the data manually; we can use Selenium Webdriver and Beautiful Soup to automate navigating across the website and collecting of the relevant data. In this code clinic, I will go through the best practices (and what not to do!) when web scraping; using Selenium Webdriver to navigate around a website and then using Beautiful Soup to extract the data from the HTML.
 # 
-# ### Introduction: Why automate? 
+
+# ### Introduction: Why automate?
+
+from random import randint
+from time import sleep
+
+import pandas as pd
+# Example 1 - Get Library books
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+url = "https://libcat.oxfordshire.gov.uk/web/arena/results?p_p_id=crDetailWicket_WAR_arenaportlet&p_p_lifecycle=1&p_p_state=normal&p_r_p_arena_urn%3Aarena_search_item_id="
+# Name /Author :[arena_search_item_id]
+books = {
+"The kiwi's egg : Charles Darwin and natural selection / David Quammen" :['0297845691&p'],
+'Blueprint : how DNA makes us who we are / Robert Plomin' :['0241282071&p', '0141984260&p'],
+'Human compatible : artificial intelligence and the problem of control / Stuart Russell' :['0241335205&p'],
+'Fake law : the truth about justice in an age of lies / The Secret Barrister' :['1529009944&p'],
+'Somersault / Kenzaburo Oe' :['1843540800&p']}
+
+browser = webdriver.Firefox()
+results = {}
+count = 0
+for book in books:
+    for version in books[book]:
+        browser.get(url + version)
+        sleep(6.25 + (randint(1,50) / 25))
+        html_source = browser.page_source
+        soup = BeautifulSoup(html_source)
+        
+        for foo in soup.find_all('div', attrs={'class': 'arena-holding-container clearfix'}):
+            if foo.text != 'Oxfordshire':
+                results[count] = {}
+                results[count]['Book'] = book.split(' / ')[0]
+                results[count]['Author'] = book.split(' / ')[1]
+                results[count]['Library'] = foo.text.replace(' Library', '').strip()
+                results[count]['Status'] = foo.find_next('div', attrs={'class': 'arena-availability-right'}).text.replace('\n', '')
+                if foo.find_previous('div', attrs={'class': 'arena-detail-class'}) != None:
+                    results[count]['Classification'] = foo.find_previous('div', attrs={'class': 'arena-detail-class'}).text.split(': ')[1].replace(' ', '')
+                else:
+                    results[count]['Classification'] = 'Not known'
+                count += 1
+        
+df = pd.DataFrame(results).T
+print(df)
+
 # ### Section 0: Load the packages and open a remote controlled browser
 
 # Import the necessary packages
@@ -11,7 +56,7 @@ from selenium import webdriver
 import requests
 
 # Fire up Selenium webdriver with the website we want to scrape
-browser = webdriver.Chrome() # If you use a different browser, replace Chrome with this
+browser = webdriver.Firefox() # If you use a different browser, replace Firefox with this
 base_url = "https://www.literaryclock.com/"
 browser.get(base_url)
 # # Alternatively, use requests
@@ -37,6 +82,7 @@ browser.find_element_by_link_text("The Literary Clock Library").click()
 
 # Use javascript to click on the element instead
 element = browser.find_element_by_link_text("The Literary Clock Library")
+print(element)
 browser.execute_script("arguments[0].click();", element)
 
 # And now to the 1500s
@@ -44,7 +90,7 @@ browser.find_element_by_link_text("1500s").click()
 
 # Can we grab the plays first performed in the 1500s
 from bs4 import BeautifulSoup
-from time import sleep
+
 html_source = browser.page_source
 soup = BeautifulSoup(html_source, 'html.parser')
 # # Alternative with requests
@@ -180,10 +226,50 @@ for century in ['/1500', '/1600', '/1700']:
 df = pd.DataFrame(books).T
 print(df)
 
-# Now for the 1800s, 1900s and 2000s - For students to do
+# Now for the 1800s, 1900s and 2000s
+for century in ['/1800', '/1900', '/2000']:
+    browser.get(base_url + 'library' + century)
+    html_source = browser.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
+    
+#     # Alternative with requests
+#     html = requests.get(base_url + 'library' + century, timeout=1)
+#     soup = BeautifulSoup(html.text, 'html.parser')
+    
+    to_do = []
+    for i in soup.findAll('a'):
+        if '/library' + century in i['href']:
+            to_do.append(i['href'])
+    
+    for period in to_do:
+        browser.get(base_url + period)
+        html_source = browser.page_source
+        soup = BeautifulSoup(html_source, 'html.parser')
+        
+#         # Alternative with requests
+#         html = requests.get(base_url + period, timeout=1)
+#         soup = BeautifulSoup(html.text, 'html.parser')
+        
+        for i in soup.findAll('li'):
+            if i.a == None:
+                books[counter] = {}
+                book = i.string
+                # print(book)
+                books[counter]['Year'] = int(book[:4])
+                temp = book[6:].split(' by ')
+                books[counter]['Title'] = temp[0]
+                books[counter]['Author'] = temp[1]
+                counter +=1
+        
+        sleep(2)
+df = pd.DataFrame(books).T
 
 # Look to quickly plot the distribution of years
 df['Year'] = df['Year'].astype('int')
 df.hist(column='Year', bins=80)
+
+df
+
+to_do
 
 
